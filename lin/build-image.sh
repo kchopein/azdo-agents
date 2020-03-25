5c97d1bf5edd0634bb67bb03af6a7ece15805063
Alex de Groot <alexdegroot@MacBook-Pro-van-Administrator.local>
2020-03-25 16:47:01 +0100

    Remove bash file

--- lin/build-image.sh
+++
@@ -1,5 +0,0 @@
-docker build -t alexdegrootnl/azdo-agent-lin:latest -t alexdegrootnl/azdo-agent-lin:202002192050 .
-
-
-
-docker run -e AZP_URL=https://dev.azure.com/raet/ -e AZP_TOKEN="jbzzbktrawy75fge34xeitbfbum6v3n7by5xuliapcde4swecnpq" -e AZP_POOL="TeamManaged - Expense Management" -e AZP_AGENT_NAME=testagent dockeragent:latest
